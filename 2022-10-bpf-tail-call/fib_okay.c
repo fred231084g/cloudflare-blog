bbd6c2b40d02a942afad64c36c96e30720738a4a
fred231084g <freddy1984govoni@gmail.com>
2022-11-02 23:08:56 +0100

    Delete 2022-10-bpf-tail-call directory

--- 2022-10-bpf-tail-call/fib_okay.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-uint64_t fib(uint64_t n)
-{
-	if (n == 0 || n == 1)
-		return 1;
-
-	return fib(n - 1) + fib(n - 2);
-}
--- 2022-10-bpf-tail-call/trace_rsp.gdb
+++
@@ -1,9 +0,0 @@
-set debuginfod enabled off
-break fib
-commands
-  silent
-  printf "n = %d, %%rsp = %#x\n", n, $rsp
-  continue
-end
-
-run
